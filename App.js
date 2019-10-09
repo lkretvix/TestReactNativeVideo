130824e42fe47b5bf37997f33240a623a216a175
lucas.kretvix <lucas.kretvix@datadoghq.com>
2019-10-08 22:38:03 -0400

    Extract Video and Controls code to components/VideoWithControls

--- App.js
+++ App.js
@@ -1,131 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  StatusBar,
-  Dimensions,
-  TouchableWithoutFeedback,
-  Text,
-} from 'react-native';
+import {SafeAreaView, StyleSheet, ScrollView, View} from 'react-native';
 
-import Video from 'react-native-video';
-import ProgressBar from 'react-native-progress/Bar';
-import Icon from 'react-native-vector-icons/FontAwesome';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-
-function secondsToTime(time) {
-  return `${Math.floor(time / 60)} : ${time % 60 < 10 ? '0' : ''} ${time % 60}`;
-}
-const RATES = [0.25, 0.5, 1.0, 1.25, 1.5, 2.0];
-const DEFAULT_SPEED = 1.0;
+import VideoWithControls from './components/VideoWithControls';
 
 export default class App extends Component {
-  state = {
-    paused: true,
-    progress: 0,
-    duration: 0,
-    rate: DEFAULT_SPEED,
-  };
-
-  handlePlayPausePress = () => {
-    if (this.state.progress >= 1) {
-      this.player.seek(0);
-    }
-    this.setState(state => {
-      return {paused: !state.paused};
-    });
-  };
-  handleProgressPress = e => {
-    const position = e.nativeEvent.locationX;
-    const progress = (position / 250) * this.state.duration;
-    this.player.seek(progress);
-  };
-  handleRateTouch = () => {
-    this.setState({
-      rate:
-        RATES[
-          (RATES.findIndex(rate => rate === this.state.rate) + 1) % RATES.length
-        ],
-    });
-  };
-  handleEnd = () => {
-    this.setState({paused: true, progress: 1});
-  };
-  handleProgress = progress => {
-    this.setState({
-      progress: progress.currentTime / this.state.duration,
-    });
-  };
-  handleLoad = meta => {
-    this.setState({
-      duration: meta.duration,
-    });
-  };
-
   render() {
-    const {width} = Dimensions.get('window');
-    const height = width * 0.5265;
     return (
       <View>
-        <StatusBar barStyle="dark-content" />
         <SafeAreaView>
           <ScrollView
             contentInsetAdjustmentBehavior="automatic"
             style={styles.scrollView}>
-            <View style={{height: height + 48}}>
-              <TouchableWithoutFeedback onPress={this.handlePlayPausePress}>
-                <Video
-                  source={require('./EdPowerBreak.mp4')}
-                  paused={this.state.paused}
-                  rate={this.state.rate}
-                  resizeMode="contain"
-                  onLoad={this.handleLoad}
-                  onProgress={this.handleProgress}
-                  onEnd={this.handleEnd}
-                  ref={ref => (this.player = ref)}
-                  style={{height, width: '100%'}}
-                />
-              </TouchableWithoutFeedback>
-            </View>
-            <View style={styles.controls}>
-              <TouchableWithoutFeedback onPress={this.handlePlayPausePress}>
-                <Icon
-                  name={!this.state.paused ? 'pause' : 'play'}
-                  size={30}
-                  color={'#FFF'}
-                />
-              </TouchableWithoutFeedback>
-              <TouchableWithoutFeedback onPress={this.handleProgressPress}>
-                <View>
-                  <ProgressBar
-                    progress={this.state.progress}
-                    color="#FFF"
-                    unfilledColor="rgba(255,255,255,0.5)"
-                    borderColor="#FFF"
-                    width={250}
-                    height={20}
-                  />
-                </View>
-              </TouchableWithoutFeedback>
-              <Text style={styles.duration}>
-                {secondsToTime(
-                  Math.floor(this.state.progress * this.state.duration),
-                )}
-              </Text>
-              <TouchableWithoutFeedback onPress={this.handleRateTouch}>
-                <Text style={styles.rate}>{`${this.state.rate}x`}</Text>
-              </TouchableWithoutFeedback>
-            </View>
+            <VideoWithControls />
           </ScrollView>
         </SafeAreaView>
       </View>
@@ -136,30 +23,5 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  controls: {
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    height: 48,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    position: 'absolute',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    paddingHorizontal: 10,
-  },
-  mainButton: {
-    marginRight: 15,
-  },
-  duration: {
-    color: '#FFF',
-    marginLeft: 15,
-  },
-  rate: {
-    color: '#FFF',
   },
 });
