3756e5213a67e8cab783bc8bb2c46c0c9b366d2c
lucas.kretvix <lucas.kretvix@datadoghq.com>
2019-10-09 11:46:15 -0400

    Move categories to premade_data/categories.js

---
+++ premade_data/categories.js
@@ -0,0 +1,28 @@
+const BASICS = {
+  title: 'Basic Stuff',
+  content: {
+    BELT_TYING: {title: 'Belt Tying'},
+    PHONE_SETUP: {title: 'Phone Setup'},
+  },
+};
+const STANCES = {
+  title: 'Stances',
+  content: {
+    STANCE1: {
+      title: 'Stance 1',
+      video: require('../assets/videos/EdConcretePunch.mp4'),
+    },
+    STANCE2: {
+      title: 'Stance 2',
+      video: require('../assets/videos/EdPowerBreak.mp4'),
+    },
+    STANCE3: {title: 'Stance 3'},
+    STANCE4: {title: 'Stance 4'},
+    STANCE5: {title: 'Stance 5'},
+  },
+};
+
+export const MAIN_CATEGORIES = {
+  BASICS: BASICS,
+  STANCES: STANCES,
+};
